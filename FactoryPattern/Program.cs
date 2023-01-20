b4085fb80255352110ad4f9e6d3160d0c3592960
Spencer Hansen <spencerhansen@gmail.com>
2023-01-20 10:15:09 -0700

    finished 1st exercise

--- FactoryPattern/Program.cs
+++ FactoryPattern/Program.cs
@@ -1,10 +1,24 @@
-﻿namespace FactoryPattern
+﻿using System;
+namespace FactoryPattern
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int NumberOfWheels;
+            bool input = false;
+            do
+            {
+                Console.WriteLine("Enter the number of tires you want for the vehicle to have");
+
+                input = int.TryParse(Console.ReadLine(), out NumberOfWheels);
+            } while (input==false);
+
+            var vehicle = VehicleFactory.GetVehicle(NumberOfWheels);
+            vehicle.Drive();
+                       
+            
+            
         }
     }
-}
+}   
